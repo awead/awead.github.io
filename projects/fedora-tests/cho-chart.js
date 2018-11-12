4cdbc051505634cc081a0de5b510a18134c80422
Adam Wead <amsterdamos@gmail.com>
2018-11-12 16:48:11 -0500

    Updating CHO metrics

--- projects/fedora-tests/cho-chart.js
+++ projects/fedora-tests/cho-chart.js
@@ -2,15 +2,23 @@ window.onload = function() {
 
   // Comparison Charts
 
+  $.get("/projects/fedora-tests/data/cho_metrics_collection_1000000_mvp4.csv", function(data) {
+    render_individual_chart("collectionComparison", data)
+  });
+
   $.when(
-    $.get("/projects/fedora-tests/data/cho_collections_1000000_solr5.csv"),
-    $.get("/projects/fedora-tests/data/cho_collections_1000000_solr7.csv")
-  ).done(function(solr_5, solr_7) {
+    $.get("/projects/fedora-tests/data/cho_metrics_work_100_mvp1.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_work_100_mvp2.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_work_100_mvp3.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_work_100_mvp4.csv")
+  ).done(function(mvp1, mvp2, mvp3, mvp4) {
     var chart = new CanvasJS.Chart(
-      "collectionComparison",
+      "workComparison",
       combined_results([
-        { label: "Solr 5", data: solr_5[0] },
-        { label: "Solr 7", data: solr_7[0] }
+        { label: "CHO 0.1", data: mvp1[0] },
+        { label: "CHO 0.2", data: mvp2[0] },
+        { label: "CHO 0.3", data: mvp3[0] },
+        { label: "CHO 0.4", data: mvp4[0] }
       ])
     );
     chart.render();
@@ -18,16 +26,30 @@ window.onload = function() {
 
   $.when(
     $.get("/projects/fedora-tests/data/postgres_collections_solr_hyrax_ns_100000.csv"),
-    $.get("/projects/fedora-tests/data/cho_metrics_collection_100000_mvp3.csv")
-  ).done(function(valkyrie, cho) {
-    var chart = new CanvasJS.Chart(
-      "choComparison",
+    $.get("/projects/fedora-tests/data/cho_metrics_collection_100000_mvp1.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_collection_100000_mvp2.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_collection_100000_mvp3.csv"),
+    $.get("/projects/fedora-tests/data/cho_metrics_collection_100000_mvp4.csv")
+  ).done(function(valkyrie, mvp1, mvp2, mvp3, mvp4) {
+    var mvpChart = new CanvasJS.Chart(
+      "choMVPComparison",
       combined_results([
-        { label: "Valkyrie", data: valkyrie[0] },
-        { label: "CHO MVP 3", data: cho[0] }
+        { label: "CHO 0.1", data: mvp1[0] },
+        { label: "CHO 0.2", data: mvp2[0] },
+        { label: "CHO 0.3", data: mvp3[0] },
+        { label: "CHO 0.4", data: mvp4[0] },
       ])
     );
-    chart.render();
+    mvpChart.render();
+
+    var currentChart = new CanvasJS.Chart(
+      "choCurrentComparison",
+      combined_results([
+        { label: "Valkyrie", data: valkyrie[0] },
+        { label: "CHO 0.4", data: mvp4[0] },
+      ])
+    );
+    currentChart.render();
   });
 
 }
--- projects/fedora-tests/cho.md
+++ projects/fedora-tests/cho.md
@@ -11,20 +11,54 @@ permalink: /cho-benchmarks
 <script type="text/javascript" src="/projects/fedora-tests/chart.js"></script>
 <script type="text/javascript" src="/projects/fedora-tests/cho-chart.js"></script>
 
-# Performance Comparison
+## Collection Tests
 
-CHO's current MVP release when compared against the original Valkyrie sample. The original
-Valkyrie test with Postgres took 7'23". In CHO, the same collection size took 43 minutes.
-CHO's collections contained randomized metadata, while the Valkyrie original did not.
+Creating a single collection with 100,000 works in it. Each work is metadata-only and has no binary
+content. The metadata is randomly generated and does differ between each work.
 
-CHO's tests also took place to a networked Solr instance, so latency could have a played a role.
+### Overall
 
-<div id="choComparison" style="width:100%; height:400px;"></div>
+| Release     | Time (sec.)     |
+| ----------- | --------------: |
+| 0.1         | 33527.1         |
+| 0.2         | 50079.7         |
+| 0.3         | 50037.7         |
+| 0.4         | 8182.1          |
 
-# Large Collections
+Network issues with Solr likely played a role in the differences between timings in release 0.1 - 0.3
+versus 0.4.
 
-Sample collection with 1 million unordered works. This is a metadata-only collection, with no files and
-only titles. It took approximately 2.5 hours to complete the test.
+### Comparing Each Release
+
+<div id="choMVPComparison" style="width:100%; height:400px;"></div>
+
+### Current Release
+
+This compares the current release with the original Valkyrie dataset.
+
+<div id="choCurrentComparison" style="width:100%; height:400px;"></div>
+
+## Works with Files
+
+Creates work with the same randomized metadata as the collection test, but also adds a 50 MB
+random file to the work.
+
+### Overall
+
+| Release     | Time (sec.)     |
+| ----------- | --------------: |
+| 0.1         | 142.0           |
+| 0.2         | 140.7           |
+| 0.3         | 153.8           |
+| 0.4         | 285.3           |
+
+### Comparing Each Release
+
+<div id="workComparison" style="width:100%; height:400px;"></div>
+
+## Large Collections
+
+Sample collection with 1 million unordered works, no binary files, and randomized metadata.
+It took approximately 26'15" to finish. This is with the current 0.4 release.
 
 <div id="collectionComparison" style="width:100%; height:400px;"></div>
-
